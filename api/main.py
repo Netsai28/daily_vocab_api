9e1c04f9969d973f0fba2f765b2928a9eeccdc15
Netsai <Netsaibu@gmail.com>
2025-11-01 11:10:11 +0700

    daily_vocab_api

--- api/main.py
+++ api/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from app.schemas import WordResponse
+from fastapi import HTTPException
+from app.routers import words
 
 # Initialize FastAPI app
 app = FastAPI(
@@ -7,18 +10,34 @@ app = FastAPI(
     description="API for vocabulary practice and learning"
 )
 
-@app.get("/api/word")
-def get_random_word():
-    """Get a random word"""
-    # TODO Write logic here....
-    return {
-        "word": "example",
-        "definition": "a representative form or pattern",
-        "difficulty_level": "Beginner"
-    }
+app.include_router(words.router, prefix="/api", tags=["words"])
 
-@app.get("/")
-def read_root():
+# @app.get("/api/word",response_model=WordResponse)
+# def get_random_word():
+#     """Get a random word"""
+
+#     words= []
+#     if len(words) ==0:
+#         raise HTTPException(
+#             status_code=404,
+#             detail="No words available in database"
+#         )
+
+# @app.get("/api/word", response_model=WordResponse)
+# def get_random_word():
+#     """Get a random word"""
+#     # TODO Write logic here....
+#     return WordResponse(
+#         id=12,
+#         word= 'book',
+#         definition= 'Readind material',
+#         difficulty_level= 'Beginner'
+#     )
+
+
+
+
+def read_root_word():
     return {
         "message": "Vocabulary Practice API",
         "version": "1.0.0",
