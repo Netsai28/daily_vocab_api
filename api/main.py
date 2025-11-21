4c14e0ad26f4942a387d99ad05f9339366d8af26
Netsai <Netsaibu@gmail.com>
2025-11-21 20:06:11 +0700

    netsai28

--- api/main.py
+++ api/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from app.schemas import WordResponse
-from fastapi import HTTPException
-from app.routers import words
+from app.database import engine, Base
+from app.models import PracticeSubmission
 
+
+Base.metadata.create_all(bind=engine)
 # Initialize FastAPI app
 app = FastAPI(
     title="Vocabulary Practice API",
@@ -10,34 +12,21 @@ app = FastAPI(
     description="API for vocabulary practice and learning"
 )
 
-app.include_router(words.router, prefix="/api", tags=["words"])
+from fastapi.middleware.cors import CORSMiddleware
 
-# @app.get("/api/word",response_model=WordResponse)
-# def get_random_word():
-#     """Get a random word"""
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-#     words= []
-#     if len(words) ==0:
-#         raise HTTPException(
-#             status_code=404,
-#             detail="No words available in database"
-#         )
+from app.routers import words
+app.include_router(words.router , prefix="/api" , tags = ["words"])
 
-# @app.get("/api/word", response_model=WordResponse)
-# def get_random_word():
-#     """Get a random word"""
-#     # TODO Write logic here....
-#     return WordResponse(
-#         id=12,
-#         word= 'book',
-#         definition= 'Readind material',
-#         difficulty_level= 'Beginner'
-#     )
-
-
-
-
-def read_root_word():
+@app.get("/")
+def read_root():
     return {
         "message": "Vocabulary Practice API",
         "version": "1.0.0",
